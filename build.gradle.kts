a9eebe64598103ba017dc0455a9e5157bcabac18
Kosty Manuhin <kostymanuhin@gmail.com>
2023-11-28 16:46:40 +0300

    dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,9 +22,15 @@ repositories {
 }
 
 dependencies {
+	// spring
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-data-rest")
+	implementation("org.springframework.boot:spring-boot-starter-security")
+
 	compileOnly("org.projectlombok:lombok")
 	runtimeOnly("org.postgresql:postgresql")
+
 	annotationProcessor("org.projectlombok:lombok")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
